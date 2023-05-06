4e02f9eefd31e4c13f1f5435553fed02b427024d
Ibrohim1702 <88259165+Ibrohim1702@users.noreply.github.com>
2023-05-06 23:33:29 +0500

    Weather
    Weather is almost finished

--- v1/weather/main.py
+++ v1/weather/main.py
@@ -7,12 +7,12 @@ url = f"https://www.google.co.uz/search?q=weather+{user_input}"
 
 r = s.get(url, headers={'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36'})
 
-temp = r.html.find('span#twob_tm', first=True).number
+temp = r.html.find('span#wob_tm', first=True).text
 unit = r.html.find('div.vk_bk.wob-unit span.wob_t', first=True).text
-desc = r.html.find('div.VQF4g', first=True). find('spanttwob_de', first=True).text
+desc = r.html.find('div.VQF4g', first=True). find('span#wob_dc', first=True).text
 
 
-print(user_input, temp, unit, desc)
+print(temp, unit, desc, user_input)
 
 
 
