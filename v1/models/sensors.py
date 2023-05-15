dd5b6cf585cb7cdb739bda00af37c8df38d354bd
jahon0088 <127406327+jahon0088@users.noreply.github.com>
2023-05-15 15:45:28 +0500

    sensor model changed, sensor api created

--- v1/models/sensors.py
+++ v1/models/sensors.py
@@ -5,5 +5,7 @@ from v1.models.geo import  District
 class SensorData(models.Model):
     sensor_id = models.BigIntegerField()
     name = models.CharField(max_length=128)
-    value = models.SmallIntegerField()
-    district = models.ForeignKey(District, on_delete=models.CASCADE)
+    tuproq_nam1 = models.JSONField()
+    tuproq_nam2 = models.JSONField()
+    havo_nam = models.JSONField()
+    havo_temp = models.JSONField()
--- v1/services/sensor.py
+++ v1/services/sensor.py
@@ -9,17 +9,31 @@ def sensor_all(requests, params):
 
 
 def sensor_add(requests, params):
-    if 'sensors' not in params:
-        return custom_response(False, message={"Error": "Data isn't enough"})
 
-    if params['sensors'] is not list:
-        return custom_response(False, message={"Error": "Sensors are not in list format"})
+    if "sensor_id" not in params:
+        return custom_response(False, custom_response({"Error":"No sensor ID"}))
 
-    if not params['sensors']:
-        return custom_response(False, message={"Error": "List is empty"})
+    if 'tuproq_nam1' not in params:
+        return custom_response(False, message={"Error": "Namlik_1 not in data"})
 
-    data = requests.data
-    sensordata = SensorData.objects.create(sensor_id=data['sensors'], name=data['name'])
+    if 'tuproq_nam2' not in params:
+        return custom_response(False, message={"Error": "Namlik_2 not in data"})
+
+    if 'havo_nam' not in params:
+        return custom_response(False, message={"Error": "havo_nam not in data"})
+
+    if "havo_temp" not in params:
+        return custom_response(False, message={"Error": "havo_temp not in data"})
+
+    # if params['sensors'] is not list:
+    #     return custom_response(False, message={"Error": "Sensors are not in list format"})
+    #
+    # if not params['sensors']:
+    #     return custom_response(False, message={"Error": "List is empty"})
+
+    sensordata = SensorData.objects.create( sensor_id=params["sensor_id"],tuproq_nam1=params['tuproq_nam1'], tuproq_nam2=params['tuproq_nam2'],
+                                           havo_nam=params["havo_nam"], havo_temp=params["havo_temp"])
+
     sensordata.save()
 
     return custom_response(True, data={"Success":"Data added "})
